8bc01b9f896043217d1baf13b7f50504c3284359
dnakano <dnakano@student.42tokyo.jp>
2021-06-16 10:08:39 +0900

    feat: syntax highlight of code block

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,9 +1,12 @@
 import { GetStaticPaths, GetStaticProps } from "next";
-import { utcStringToDateString } from "../../libs/dates";
+import cheerio from "cheerio";
+import hljs from "highlight.js";
 import { PostAPIRepository } from "../../modules/api/post";
 import { Post } from "../../modules/entity/post";
 import CategoryLink from "../../components/CategoryLink";
 import DateInfo from "../../components/DateInfo";
+import "highlight.js/styles/base16/Material.css";
+import "tailwindcss/tailwind.css";
 
 export default function PostArticle({ postData }: { postData: Post }) {
   return (
@@ -21,10 +24,13 @@ export default function PostArticle({ postData }: { postData: Post }) {
         />
       </div>
 
-      <hr className="mx-5"/>
+      <hr className="mx-5" />
 
       <article className="py-5">
-        <div className="post" dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <div
+          className="post"
+          dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+        />
       </article>
     </>
   );
@@ -49,6 +55,20 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const repo = new PostAPIRepository();
   const postData = await repo.fetchPostById(params.id as string);
+
+  // add syntax highlight to post html
+  // https://blog.microcms.io/syntax-highlighting-on-server-side/
+  const $ = cheerio.load(postData.contentHtml);
+  $("code").addClass("px-1 bg-gray-200");
+  $("pre code").addClass("text-sm");
+  $("pre code").addClass("rounded-md");
+  $("pre code").each((_, elm) => {
+    const result = hljs.highlightAuto($(elm).text());
+    $(elm).html(result.value);
+    $(elm).addClass("hljs");
+  });
+  postData.contentHtml = $.html();
+
   return {
     props: {
       postData,
